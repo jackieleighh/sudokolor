e2a8aa56e220170e249e8a3f7cdf0cf7c979f401
Jacqueline Harnish <jacquelineharnish@jacquelines-mbp.myfiosgateway.com>
2022-02-03 18:05:11 -0500

    add levels, modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { makePuzzle, pluck } from './utils';
 
 // material
 import { styled } from '@mui/material/styles';
-import { Grid, Button, Typography } from '@mui/material';
+import { Grid, Button, Typography, Modal, Box, FormControl, InputLabel, Select, MenuItem, SelectChangeEvent } from '@mui/material';
 import ClearIcon from '@mui/icons-material/Clear';
 
 // TODO - add themes
@@ -19,15 +19,30 @@ const COLOR_MAP = [
   '#e00ac6'
 ];
 
+const modalStyle = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '1px solid #000',
+  boxShadow: 2,
+  p: 2,
+};
+
 function App() {
 
   const [board, setBoard] = useState<number[][] | null>(null);
   const [puzzle, setPuzzle] = useState<number[][] | null>(null);
   const [solution, setSolution] = useState<number[][] | null>(null);
   const [selected, setSelected] = useState([-1, -1]);
+  const [wrong, setWrongCount] = useState(0);
+  const [modalOpen, setModalOpen] = useState(true);
+  const [level, setLevel] = useState('20');
 
-  // TODO - add levels (easy, hard, etc)
-  const generateGame = (pluckCount = 20) => {
+  // TODO - use wrong somehow - hint reveals how many wrong/hightlights wrong square?
+  const generateGame = () => {
     // make a puzzle solution
     const solution = makePuzzle();
     // reset selected
@@ -37,17 +52,13 @@ function App() {
       // copy to make solution immutable
       const solutionCopy = Array.from(solution).map(el=>Array.from(el));
       // pluck values from cells to create the game
-      const board = pluck(solutionCopy, pluckCount);
+      const board = pluck(solutionCopy, parseInt(level, 10));
       // copy so board changes do not update puzzle
       const puzzle = Array.from(board).map(el => Array.from(el));
       setPuzzle(puzzle);
       setBoard(board);
     }
   }
-
-  useEffect(() => {
-    generateGame();
-  }, []);
 
   const handleSelectSquare = (i: number, j: number) => {
     // check if it is part of the puzzle
@@ -58,10 +69,11 @@ function App() {
   const handlePutColor = (color: number) => {
     const row = selected[0], col = selected[1];
     if(!solution || !board || row < 0 || col < 0) return;
-    if(solution[row][col] === (color + 1)) {
-      // congrats! you got a square
-      board[row][col] = color + 1;
-      setBoard([...board]);
+    // congrats! you got a square
+    board[row][col] = color + 1;
+    setBoard([...board]);
+    if(solution[row][col] !== (color + 1)) {
+      setWrongCount(wrong + 1);
     }
   };
 
@@ -74,6 +86,23 @@ function App() {
       board[row][col] = -1;
       setBoard([...board]);
     }
+  }
+
+  const handleModalClose = () => {
+    setModalOpen(false);
+  };
+
+  const handleLevelChange = (e: SelectChangeEvent<string>) => {
+    setLevel(e.target.value);
+  };
+
+  const handleStartGame = () => {
+    setModalOpen(false);
+    generateGame();
+  };
+
+  const handleNewGame = () => {
+    setModalOpen(true);
   }
 
   return (
@@ -96,10 +125,10 @@ function App() {
                 {row.map((cell: number, j: number) => (
                   <div 
                     key={j} 
-                    className="puzzleCell" 
+                    className={`puzzleCell ${puzzle && puzzle[i][j] === 0 && board && board[i][j] !== 0 ? 'guess': null}`}
                     style={{ 
                       background: COLOR_MAP[cell - 1], 
-                      border: selected[0] === i && selected[1] === j ? '2px solid black' : '2px solid white'
+                      boxShadow: selected[0] === i && selected[1] === j ? 'inset 0px 0px 0px 2px #000' : 'none'
                     }} 
                     onClick={() => handleSelectSquare(i, j)}
                     /> 
@@ -118,20 +147,55 @@ function App() {
                   onClick={() => handlePutColor(i)} 
                 />
               ))}
-              <button className="colorButton erase" onClick={() => handleErase()}>
+              <button className="colorButton erase" onClick={handleErase}>
                 <StyledClearIcon />
               </button>
               </div>
             </Grid>
             <Grid item xs={12}>
               <div className="controls">
-                <ControlButton variant="outlined" onClick={() => generateGame()}>
+                <ControlButton variant="outlined" onClick={handleNewGame}>
                   new game
                 </ControlButton>
               </div>
             </Grid>
           </Grid>
         </Grid>
+        <Modal
+          open={modalOpen}
+          onClose={handleModalClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={modalStyle}>
+            <div style={{ textAlign: 'center', marginBottom: '10px' }}>
+              <Typography id="modal-modal-title" variant="h6">
+                sudokolor!
+              </Typography>
+              <Typography variant="caption" display="block" gutterBottom>a color-based sudoku game</Typography>
+            </div>
+            <div>
+            <FormControl variant="standard" sx={{ m: 1 }} fullWidth>
+            <InputLabel id="level-select-label">level</InputLabel>
+            <Select
+              labelId="level-select-label"
+              id="level"
+              value={level}
+              label="Level"
+              onChange={handleLevelChange}
+            >
+              <MenuItem value={20}>easy</MenuItem>
+              <MenuItem value={30}>not too bad</MenuItem>
+              <MenuItem value={40}>hard</MenuItem>
+              <MenuItem value={50}>really hard</MenuItem>
+              <MenuItem value={60}>almost impossible</MenuItem>
+            </Select>
+          </FormControl>
+          <div style={{ textAlign: 'center', marginTop: '10px' }}>
+          <ControlButton variant="outlined" onClick={handleStartGame}>let&apos;s go</ControlButton>
+            </div></div>
+          </Box>
+        </Modal>
     </ContainerGrid>
   );
 }
