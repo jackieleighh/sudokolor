52347cf6c960cc90028bbc1322c91f784632dfe7
Jacqueline Harnish <jacquelineharnish@jacquelines-mbp.myfiosgateway.com>
2022-02-03 13:24:29 -0500

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,109 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { makePuzzle, pluck } from './utils';
+import { Grid, Button, ButtonGroup } from '@mui/material';
+
+// TODO - add themes
+const COLOR_MAP = [
+  '#d40740',
+  '#db5e44',
+  '#c9a904',
+  '#179252',
+  '#68e869',
+  '#52c5df',
+  '#0e57b0',
+  '#7748d4',
+  '#e00ac6'
+];
 
 function App() {
+
+  const [board, setBoard] = useState<number[][] | null>(null);
+  const [puzzle, setPuzzle] = useState<number[][] | null>(null);
+  const [solution, setSolution] = useState<number[][] | null>(null);
+  const [selected, setSelected] = useState([-1, -1]);
+
+  // TODO - add levels (easy, hard, etc)
+  const generateGame = (pluckCount = 20) => {
+    // make a puzzle solution
+    const solution = makePuzzle();
+    // reset selected
+    setSelected([-1,-1]);
+    if (solution) {
+      setSolution(solution);
+      // copy to make solution immutable
+      const solutionCopy = Array.from(solution).map(el=>Array.from(el));
+      // pluck values from cells to create the game
+      const board = pluck(solutionCopy, pluckCount);
+      // copy so board changes do not update puzzle
+      const puzzle = Array.from(board).map(el => Array.from(el));
+      setPuzzle(puzzle);
+      setBoard(board);
+    }
+  }
+
+  useEffect(() => {
+    generateGame();
+  }, []);
+
+  const handleSelectSquare = (i: number, j: number) => {
+    // check if it is part of the puzzle
+    if(puzzle && puzzle[i][j] > 0) return;
+    setSelected([i, j]);
+  };
+
+  const handlePutColor = (color: number) => {
+    const row = selected[0], col = selected[1];
+    if(!solution || !board || row < 0 || col < 0) return;
+    if(solution[row][col] === (color + 1)) {
+      // congrats! you got a square
+      board[row][col] = color + 1;
+      setBoard([...board]);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid
+        container
+        spacing={0}
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
+        style={{ minHeight: '100vh' }}
+      >
+        <Grid item xs={3} style={{ maxWidth: '400px', width: '100%', padding: '0 10px' }}>
+          <Grid container>
+            <Grid item xs={12}>
+            {board && board.map((row, i) => (
+              <div key={i} className="puzzleRow">
+                {row.map((cell: number, j: number) => (
+                  <div 
+                    key={j} 
+                    className="puzzleCell" 
+                    style={{ 
+                      background: COLOR_MAP[cell - 1], 
+                      marginRight: j % 3 === 2 ? '10px' : '0px',
+                      border: selected[0] === i && selected[1] === j ? '2px solid black' : '2px solid white'
+                    }} 
+                    onClick={() => handleSelectSquare(i, j)}
+                    /> 
+                ))}
+                {i % 3 === 2 ? ( <div style={{ height: '10px' }} /> ) : null}
+              </div>
+            ))}
+            </Grid>
+            <Grid item xs={12}>
+            <ButtonGroup variant="contained">
+              {COLOR_MAP.map((c, i) => (
+                <Button key={i} style={{ background: c }} onClick={() => handlePutColor(i)} />
+              ))}
+              </ButtonGroup>
+            </Grid>
+            <Grid item xs={12}>
+              <Button onClick={() => generateGame()}>new game</Button>
+            </Grid>
+          </Grid>
+        </Grid>
+    </Grid>
   );
 }
 
---
+++ src/utils.tsx
@@ -0,0 +1,114 @@
+/**
+ * makes a puzzle
+ * @returns {Array}
+ */
+export const makePuzzle = () => {
+    let isPuzzle = false;
+    while(!isPuzzle) {
+        const puzzle: number[][] = [];
+        const rows = Array.from(Array(9).fill([1, 2, 3, 4, 5, 6, 7, 8, 9]));
+        const cols = Array.from(Array(9).fill([1, 2, 3, 4, 5, 6, 7, 8, 9]));
+        const squares = Array.from(Array(9).fill([1, 2, 3, 4, 5, 6, 7, 8, 9]));
+        let count = 0;
+        for(let i = 0; i < 9; i += 1) {
+            const p: number[] = [];
+            for(let j = 0; j < 9; j  += 1) {
+                const sqIdx = (Math.floor(i/3)* 3 + Math.floor(j/3));
+                const choices = [...rows[i]].filter(x => cols[j].includes(x)).filter(x => squares[sqIdx].includes(x));
+                const choice = choices[Math.floor(Math.random() * choices.length)];
+                if(!choice) {
+                    // we hit a dead end
+                    continue;
+                }
+                p.push(choice);
+                rows[i] = rows[i].filter((x: number) => x !== choice);
+                cols[j] = cols[j].filter((x: number) => x !== choice);
+                squares[sqIdx] = squares[sqIdx].filter((x: number) => x !== choice);
+                count += 1;
+            }
+            puzzle.push(p);
+        }
+        if(count === 81) isPuzzle = true;
+        if(isPuzzle) return puzzle;
+    }
+};
+
+/**
+ * removes one cell at a time at random, while making sure puzzle can still be solved
+ * @param puzzle 
+ * @param n 
+ * @returns {Array}
+ */
+export const pluck = (puzzle: number[][], n = 0) => {
+    const cells: number[] = Array.from(Array(81).keys());
+    let count = 0;
+    while(cells.length > n && count < n) {
+        let cell = -1;
+        while(cell === -1) {
+            cell = cells[Math.floor(Math.random() * cells.length)];
+        }
+        const x = Math.floor(cell / 9);
+        const y = cell % 9;
+        // see if we can remove this from the puzzle
+        // check if a cell in the same row, col, or square can be the plucked value
+        // if all 3 are true, we can't pluck this value
+        let checkRow = false, checkCol = false, checkSquare = false;
+        for(let i = 0; i < 9; i += 1) {
+            const row = { x: i, y };
+            const col = { x, y: i };
+            const square = {
+                x: (Math.floor(Math.floor(cell / 9) / 3) * 3) + Math.floor(i / 3),
+                y: ((Math.floor(cell / 9) % 3) * 3) + (i % 3),
+            };
+            if(row.y !== y) {
+                checkRow = canBe(puzzle, row.x, row.y, cell);
+            }
+            if(col.x !== x) {
+                checkCol = canBe(puzzle, col.x, col.y, cell);
+            }
+            if(square.x !== x && square.y !== y) {
+                checkSquare = canBe(puzzle, square.x, square.y, cell);
+            }
+        }
+        if(checkRow && checkCol && checkSquare) {
+            // removing would make puzzle unsolvable
+            continue;
+        } else {
+            // we can pluck this cell!
+            puzzle[x][y] = 0;
+            cells[cell] = -1;
+            count += 1;
+        }
+    }
+    return puzzle;
+}
+
+/**
+ * checks if the cell at (i,j) can contain the number we are trying to pluck
+ * @param puzzle 
+ * @param i 
+ * @param j 
+ * @param cell
+ * @returns 
+ */
+const canBe = (puzzle: number[][], i: number, j: number, cell: number) => {
+    const x = Math.floor(cell / 9);
+    const y = cell % 9;
+    const val = puzzle[x][y];
+
+    if(puzzle[i][j] === val) return true;
+    
+    for(let k = 0; k < 9; k += 1) {
+        const row = { x: k, y: j };
+        const col = { x: i, y: k };
+        const square = {
+            x: Math.floor(i/3) * 3 + Math.floor(k / 3),
+            y: Math.floor(j/3) * 3 + k % 3
+        };
+        if(!(row.x === x && row.y === y) && puzzle[row.x][row.y] === val) return false;
+        if(!(col.x === x && col.y === y) && puzzle[col.x][col.y] === val) return false;
+        if(!(square.x === x && square.y === y) && puzzle[square.x][square.y] === val) return false;
+    }
+
+    return true;
+}
